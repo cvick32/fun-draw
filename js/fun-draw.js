086c658bee548559602b96b20f641e8a20e72626
moonishteapot <cole.d.vick@gmail.com>
2018-05-26 18:53:43 -0400

    initial commit

---
+++ js/fun-draw.js
@@ -0,0 +1,72 @@
+var mousePoint = view.center;
+var amount = 25;
+var colors1 = ['black', 'blue', 'orange', 'red'];
+var colors2 = ['black', 'white', 'black', 'white'];
+var colors = colors1;
+var x = 1;
+var clicks = 1;
+var trail = [];
+var sticky = false;
+var rects = false;
+// making x large and clicks small makes long rectangles that twirl around each other
+//  that create circles when you leave it sticky
+// keep x = 1 and clicks = 1 for small circles
+
+function build() {
+  for (var i = 0; i < amount; i++) {
+    var rect = new Rectangle([0, 0], [x, clicks]);
+    rect.center = mousePoint;
+    var path = new Path.Rectangle(rect, 6); // make circle by adding rounded second arg
+    path.fillColor = colors[i % 4];
+    var scale = (1 - i / amount) * 20;
+    path.scale(scale);
+    trail.push(path);
+  }
+}
+
+build();
+
+function onMouseMove(event) {
+  mousePoint = event.point;
+}
+
+function onMouseDown(event) {
+  sticky = !sticky;
+}
+
+function onKeyDown(event) {
+  switch (event.key) {
+    case "=":
+      clicks += 1;
+      break;
+    case "c":
+      if (colors == colors1) {
+        colors = colors2;
+      } else {
+        colors = colors1;
+      }
+      break;
+    case "-":
+      clicks = clicks - 1;
+    case "r":
+      rects = !rects;
+    default:
+      break;
+  }
+}
+
+function onFrame(event) {
+  for (var i = 0, l = trail.length; i < l; i++) {
+    var item = trail[i];
+    var delta = (mousePoint - item.position) / (i + 5);
+    item.rotate(Math.sin((event.count + i) / 10) * 7);
+    item.position += delta;
+    item.fillColor = colors[i % 4];
+    item.scale = clicks;
+    if (sticky) {
+      var clone = item.clone();
+      clone.scale(clicks);
+      clone.fillColor = colors[i % 4];
+    }
+  }
+}
