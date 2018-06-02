8047f7f7e55b2b714c3f60b9aa0343bd4a8a916e
moonishteapot <cole.d.vick@gmail.com>
2018-06-02 19:50:06 -0400

    node css changes

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+app.use(express.static(__dirname + "/css/"));
+app.use(express.static(__dirname + "/html/"));
+app.use(express.static(__dirname + "/node_modules/"));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/html/canvas.html'));
     console.log(__dirname);
